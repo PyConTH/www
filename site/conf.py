99cd3689350c095eb8517506045629b5a50fc719
Jean Jordaan <jean.jordaan@gmail.com>
2017-12-19 20:59:21 +0700

    Include the identity widget

--- site/conf.py
+++ site/conf.py
@@ -1206,6 +1206,7 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # before </head>
 # (translatable)
 # EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = '<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>'
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
