018cdedc92ecbb90047570a5ab0e364425752047
Jean Jordaan <jean.jordaan@gmail.com>
2017-12-21 20:36:58 +0700

    Add site navigation

--- site/conf.py
+++ site/conf.py
@@ -133,6 +133,18 @@ TRANSLATIONS_PATTERN = '{path}.{lang}.{ext}'
 
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
+        (
+            ("/about/", "About"),
+            ("/code-of-conduct/", "Code of Conduct"),
+            "About"
+            ),
+        ),
+        (
+            ("/programme/", "Programme"),
+            ("/venue/", "Venue"),
+            "Programme"
+            ),
+        ),
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
         ("/rss.xml", "RSS feed"),
