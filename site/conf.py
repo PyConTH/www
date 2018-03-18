cc3e8ea6eeecc03b404bac7d802d579f9f7769ee
Jean Jordaan <jean.jordaan@gmail.com>
2018-03-18 16:26:40 +0700

    Use CDN for bootstrap & fontawesome

--- site/conf.py
+++ site/conf.py
@@ -1381,6 +1381,7 @@ SHOW_SOURCELINK = False
 # Set this to False if you want to host your site without requiring access to
 # external resources.
 # USE_CDN = False
+USE_CDN = True
 
 # Check for USE_CDN compatibility.
 # If you are using custom themes, have configured the CSS properly and are
