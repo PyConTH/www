6fa93b048eca719b959b46ca32c402ceee912ac0
Jean Jordaan <jean.jordaan@gmail.com>
2018-05-16 16:05:53 +0700

    Add a paragraph to get some margin after sponsors

--- site/conf.py
+++ site/conf.py
@@ -1107,7 +1107,7 @@ CONTENT_FOOTER = {
     </div>
   </div>
 </div>
-<p></p>
+<p>&nbsp;</p>
 <div class="navbar-inverse row" style="
     color: white;
 ">
@@ -1193,6 +1193,7 @@ CONTENT_FOOTER = {
     </div>
   </div>
 </div>
+<p>&nbsp;</p>
 <div class="navbar-inverse row" style="
     color: white;
 ">
