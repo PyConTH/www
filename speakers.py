e8831d12a838f0c07020560a7c01ef2bdcfa3407
Francois Wautier <francois@wautier.eu>
2019-06-08 20:04:07 +0700

    Updated talks and speakers

--- speakers.py
+++ speakers.py
@@ -4,6 +4,9 @@ import html2text
 import re
 from copy import deepcopy
 
+FILELOC="/home/fw/Downloads/"
+
+seenemail=[]
 xtratalks = []
 xtratalks.append({
     "name": "Mohamed Ali SOLA (Dali SOLA)",
@@ -27,10 +30,45 @@ This talk will go through:
     'talk_format': "Talk (~30-45 minutes)"
         })
 
+xtratalks.append({
+    "name": "Mishari Muqbil",
+    "email": "mishari@mishari.net",
+    "title": "Teaching Coding To Kids",
+"abstract": """This is a workshop for adults such as parents, teachers, community organizers and others interested in organizing classes where kids learn how to program Python in a peer to peer, collaborative learning environment.
+
+We will take a time machine back to the time when we were just starting off with coding and explore what that “aha” moments were for each of us. Based on that we will split into groups along common themes and design learning paths and a curriculum outline for kids to explore the joys of programming.
+
+Collaborative learning environments put less emphasis on instructors and more on children’s natural tendency to be curious and to share. As part of this workshop you will learn how to facilitate such an environment.
+
+Kids are welcome to join too as subject matter experts and participants in the process.
+
+Requirement:
+ - Humans aged 7+
+ - Computer, Tablet or anything that runs Python
+""",
+    "twitter":"",
+    "bio": "CEO of Zymple",
+    "avatar":"",
+    'talk_format': "Workshop (> 60 minutes)"
+        })
+
+xtratalks.append({
+    "name": "Chanapai Chuadchum",
+    "email": "teslacoil358@gmail.com",
+    "title": "How to build an open source catbot with Python, a 3D printer and a microcontroller",
+    "abstract": """This talk is about a project that was designed to test the ML A.I research and robotics opensource. The aim is to improve the robot's ability at human interaction and develop various applications for the robot. Right now this robot is an early protype with limited applications. This project aims to improve the robot's abilities in the near future.""",
+    "twitter":"",
+    "avatar":"",
+    "bio": """My real name is Chanapai Chuadchum my nick name is Korn i was started my work on roobtics since i was 10 on the and i got the thrid place on the competition in Solar innovative contest when i was 11 years old since then i'm dedicated to working on robotics to develop the useful and sophisticated robots to make them life like as most as i can currently i'm studying robotics engineer at the pensylavania university online in Robotics Engineer Master degree
+Currently i'm trying to make my own company in robotics field to serve humanity as much as possible to eleminate the time that we need to spend on unneccessary thing.""",
+    'talk_format': "Talk (~30-45 minutes)"
+        })
 
 
-talks = json.load(open('PyCon Thailand 2019 Submissions.json'))
-accepted_talks = [talk for talk in talks if talk['state'] == 'accepted' and talk["confirmed"]]
+
+talks = json.load(open(FILELOC + 'PyCon Thailand 2019 Submissions.json'))
+#accepted_talks = [talk for talk in talks if talk['state'] == 'accepted' and talk["confirmed"]]
+accepted_talks = [talk for talk in talks if talk['state'] == 'accepted']
 # Anything that isn't a square closing bracket
 name_regex = "[^]]+"
 # http:// or https:// followed by anything but a closing paren
@@ -48,7 +86,12 @@ for x in accepted_talks:
         newtalk['bio'] = """Martin has over 20 years’ experience in Machine Learning and has used it to solve problems in financial modelling and has created AI automation for companies. His current area of focus and speciality is in natural language processing and understanding. In 2017, Google appointed Martin as one of the first 12 Google Developer Experts for Machine Learning. Martin is also one of the co-founders of Red Dragon AI."""
         newtalk['avatar'] = "https://th.pycon.org/martin-andrews.jpeg"
         newtalk['twitter'] = ''
+        newtalk['email'] = 'GitHub@mdda.net'.lower()
         xtratalks.append(newtalk)
+    elif x['title'].lower() == "code like a girl":
+        x['title'] = "Girls Who Dev Code Like Godesses"
+    elif x["title"].startswith("AIoT"):
+        x["bio"] = "CEO of DycodeX - the AIoT enabler in Indonesia. Microsoft Most Valuable Professional (MVP) of Microsoft Azure. 15 years as entrepreneur. A developer, hardware maker, and public speaker. "
     else:
         x['bio'] = x['bio'].replace("<ul>","")
         x['bio'] = x['bio'].replace("</ul>","")
@@ -137,21 +180,23 @@ print(header.format(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S UTC+07:00")))
 speakers = {}
 
 for talk in accepted_talks:
-    # title
-    talk['title_underline'] = '='*len(talk['title'])
-    talk['title_block'] = title_block.format(**talk)
-    # speaker
-    talk['avatar_block'] = ""
-    if talk.get('avatar'):
-        talk['avatar_block'] = avatar_block.format(**talk)
-    talk['twitter_block'] = ""
-    if talk.get('twitter'):
-        talk['twitter_block'] = twitter_block.format(**talk)
-    talk['link_block'] = ""
-    if talk.get('link'):
-        talk['link_block'] = link_block.format(**talk)
-    talk['name_underline'] = '='*len(talk['name'])
-    talk['name_block'] = name_block.format(**talk)
+    if talk['email'].strip().lower() not in seenemail:
+        # title
+        talk['title_underline'] = '='*len(talk['title'])
+        talk['title_block'] = title_block.format(**talk)
+        # speaker
+        talk['avatar_block'] = ""
+        if talk.get('avatar'):
+            talk['avatar_block'] = avatar_block.format(**talk)
+        talk['twitter_block'] = ""
+        if talk.get('twitter'):
+            talk['twitter_block'] = twitter_block.format(**talk)
+        talk['link_block'] = ""
+        if talk.get('link'):
+            talk['link_block'] = link_block.format(**talk)
+        talk['name_underline'] = '='*len(talk['name'])
+        talk['name_block'] = name_block.format(**talk)
 
-    print(talk_page.format(**talk))
+        print(talk_page.format(**talk))
+        seenemail.append(talk['email'].strip().lower())
 
